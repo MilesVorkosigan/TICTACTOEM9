b13c59c910cf18c72767d218fbb45285ad8bfc6f
vpenyarando <adam22vpenarando@inslaferreria.cat>
2024-02-22 17:51:07 +0100

    Servidor central + Clase Partida + cliente simple de prueba

--- src/m9tictactoe/M9TicTacToe.java
+++ src/m9tictactoe/M9TicTacToe.java
@@ -1,20 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package m9tictactoe;
 
-/**
- *
- * @author MilesMarxant
- */
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class M9TicTacToe {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws UnknownHostException, SocketException, IOException {
+                Scanner teclat = new Scanner(System.in);
+		InetAddress serverAddress = InetAddress.getByName("127.0.0.1");
+		int serverPort = 7879;
+		
+                Random random = new Random();
+                int clientPort = random.nextInt(5535) + 1024;
+                
+		DatagramSocket socket = new DatagramSocket(clientPort);
+		
+		System.out.print("MSG[enter per finalitzar]: ");
+		String msg = teclat.nextLine();
+		while(!msg.isEmpty()) {
+			//enviar missatge a servidor
+			byte[] bytesOUT = msg.getBytes();
+			DatagramPacket outPacket = new DatagramPacket(bytesOUT, bytesOUT.length, serverAddress, serverPort);
+			socket.send(outPacket);
+                        System.out.println(socket.getPort());
+                        
+			
+			//rebre echo del servidor
+			byte[] buffer = new byte[1024];
+			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+			socket.receive(packet);
+			System.out.println(new String(packet.getData()).trim());
+			
+			//obtenir nou missatge de teclat
+			System.out.print("MSG[enter per finalitzar]: ");
+			msg = teclat.nextLine();
+        }
     }
-    
 }
